ecadf38a6cf103dd3059d6bc32ca47dde0a3f30e
LAPAYA_STREAMING <adgjmadag9@gmail.com>
2025-08-06 00:41:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'PUT SESSION ID HERE' : process.env.SESSION_ID, 
+SESSION_ID: process.env.SESSION_ID === undefined ? 'VISPER-MD&jlRiBYRb#XEPk8ya7TMU5I0N7yEP_2xNEvVUCZwDzKAd6UrzTNtE' : process.env.SESSION_ID, 
 PORT: process.env.PORT === undefined ? '8000' : process.env.PORT,    
 SESSION_NAME: process.env.SESSION_NAME === undefined ? 'vispermd' : process.env.SESSION_NAME, 
 
